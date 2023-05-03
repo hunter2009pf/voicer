4563b90ff6774bbca43ac687714f81bd2e9b151f
Jack Pan <angel_clothes@outlook.com>
2023-05-03 10:09:47 +0800

    initialize project

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import PySimpleGUI as sg
+
+
+# Define the window's contents
+layout = [[sg.Text("What's your name?")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Create the window
+    window = sg.Window('Window Title', layout)
+
+    # Display and interact with the Window using an Event Loop
+    while True:
+        event, values = window.read()
+        # See if user wants to quit or window was closed
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+        # Output a message to the window
+        window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
+    # Finish up by removing from the screen
+    window.close()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
